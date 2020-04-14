e050424c98c63bd6efa1c26fb6492704ab90fe03
Bolaji Olajide <cooproton@gmail.com>
2020-04-14 07:47:15 +0100

    working with strings

---
+++ working_with_strings.rb
@@ -0,0 +1,10 @@
+phrase = "  Giraffe Academy   "
+
+puts phrase
+puts phrase.upcase
+puts phrase.downcase
+puts phrase.strip
+puts phrase.strip.length # methods are sometimes automatically called
+puts phrase.include? "affe" # true
+puts phrase.include? "cold" # false
+puts phrase.strip.index('G') # tells what index the character G is
