8c0e148a004eadadff3ceef408b37cff9c6777e4
Bolaji Olajide <cooproton@gmail.com>
2020-04-24 07:21:23 +0100

    modules in ruby

---
+++ module_import.rb
@@ -0,0 +1,5 @@
+require_relative "modules.rb"
+# to make use of a module
+include Tools
+
+Tools.sayhi("Proton")
---
+++ modules.rb
@@ -0,0 +1,14 @@
+module Tools
+  def sayhi(name)
+    puts "hello #{name}"
+  end
+
+  def saybye(name)
+    puts "bye #{name}"
+  end
+end
+
+# to make use of a module
+# include Tools
+#
+# Tools.sayhi("Bimpe")
